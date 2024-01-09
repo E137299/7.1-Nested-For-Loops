23159c86b418dcf557246b199b2c04b99d2337a1
David Yin <29613693+E137299@users.noreply.github.com>
2024-01-08 20:39:39 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# These three functions will use the print() function. They will not require a return statment
+
+def rolling_dice():
+  pass
+
+def multiplication_table(n):
+  pass
+  
+def three_categories():
+  pass
